c5ad74f82f40ea0b7fdc54a46e74c704bacc8d07
Byron Mendoza <byronmendoza.geek@gmail.com>
2021-01-11 12:07:39 -0500

    Add validation to singup form and create user on firebase with email auth

---
+++ public/js/auth.js
@@ -0,0 +1,11 @@
+// //signup
+// const signupform = document.querySelector('#signup-form');
+// signupform.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     //get user info
+//     const email = signupform['signup-email'].value;
+//     const password = signupform['singup-password'].value;
+//     const password2 = signupform['signup-password2'].value;
+//     console.log(email, password, password2);
+// })
--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,22 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+	container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+	container.classList.remove("sign-up-mode");
 });
+//data to pwd validation
+const lowerCaseLetters = /[a-z]/g;
+const upperCaseLetters = /[A-Z]/g;
+const numbers = /[0-9]/g;
+const letter = document.getElementById("letter");
+const capital = document.getElementById("capital");
+const number = document.getElementById("number");
+const length = document.getElementById("length");
+
+
 
 const signupform = document.getElementById('signup-form');
 const signinform = document.getElementById('signin-form');
@@ -18,41 +28,73 @@ const signinpassword = document.getElementById('signin-password');
 const signuppassword = document.getElementById('signup-password');
 const signuppassword2 = document.getElementById('signup-password2');
 
+//signup
 signupform.addEventListener('submit', e => {
-  e.preventDefault();
-  checkInputs();
+	e.preventDefault();
+	checkInputs();
+
+	//get user info 
+	const email = signupemail.value;
+	const password = signuppassword.value;
+	const password2 = signuppassword2.value;
+	console.log(email, password, password2);
+
+	// sign up the user
+	auth.createUserWithEmailAndPassword(email, password).then(cred => {
+		console.log(cred)
+	});
 });
 
 signinform.addEventListener('submit', e => {
-  e.preventDefault();
-  checkInputs();
+	e.preventDefault();
+	checkInputs();
 });
 
 function checkInputs() {
+	
 	// trim to remove the whitespaces
+	const signingemailValue = signinemail.value.trim();
+	const signinpasswordValue = signinpassword.value.trim();
 	const signupemailValue = signupemail.value.trim();
 	const signuppasswordValue = signuppassword.value.trim();
 	const signuppassword2Value = signuppassword2.value.trim();
-	
-	if(signupemailValue === '') {
+
+	if (signupemailValue === '') {
 		setErrorFor(signupemail, 'Email cannot be blank');
 	} else if (!isEmail(signupemailValue)) {
 		setErrorFor(signupemail, 'Not a valid email');
 	} else {
 		setSuccessFor(signupemail);
 	}
-	
-	if(signuppasswordValue === '') {
+
+	if (signingemailValue === '') {
+		setErrorFor(signinemail, 'Email cannot be blank');
+	}
+
+	if (signuppasswordValue === '') {
 		setErrorFor(signuppassword, 'Password cannot be blank');
-	} else {
+	} else if (signuppassword.value.match(lowerCaseLetters)) {
+		setErrorFor(signuppassword, 'Check password rules');
+	}else if(signuppassword.value.match(upperCaseLetters)){
+		setErrorFor(signuppassword, 'Check password rules');
+	}else if(signuppassword.value.match(numbers)){
+		setErrorFor(signuppassword, 'Check password rules');
+	} else if(signuppassword.value.length <=8){
+		setErrorFor(signuppassword, 'Check password rules');
+	}
+	else {
 		setSuccessFor(signuppassword);
 	}
-	
-	if(signuppassword2Value === '') {
+
+	if (signinpasswordValue === '') {
+		setErrorFor(signinpassword, 'Password cannot be blank');
+	}
+
+	if (signuppassword2Value === '') {
 		setErrorFor(signuppassword2, 'Password2 cannot be blank');
-	} else if(signuppasswordValue !== signuppassword2Value) {
+	} else if (signuppasswordValue !== signuppassword2Value) {
 		setErrorFor(signuppassword2, 'Passwords does not match');
-	} else{
+	} else {
 		setSuccessFor(signuppassword2);
 	}
 }
@@ -68,7 +110,56 @@ function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'input-field success';
 }
-	
+
 function isEmail(signupemail) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(signupemail);
+}
+
+// When the user clicks on the password field, show the message box
+signuppassword.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+signuppassword.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+signuppassword.onkeyup = function() {
+  // Validate lowercase letters
+  
+  if(signuppassword.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+
+  if(signuppassword.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  if(signuppassword.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(signuppassword.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
 }
